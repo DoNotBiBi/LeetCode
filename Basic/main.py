8cc61da6eb1e85f22564b6e539f96da47c2fc735
wangkang <wangkang941218@outlook.com>
2020-08-17 17:57:34 +0800

    shanchu

--- Basic/main.py
+++ Basic/main.py
@@ -112,6 +112,4 @@ def cal_test():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print(__name__)  # 输出文件名
-    print(dir(__name__))  # ceshi
-
-# sdfgsrgs
+    print(dir(__name__))
