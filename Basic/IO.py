00d3dca736db63a4855bd59dd28eb6c30690b150
wangkang <wangkang941218@outlook.com>
2020-08-18 08:28:50 +0800

    添加一些str_format

--- Basic/IO.py
+++ Basic/IO.py
@@ -17,3 +17,5 @@ def strformat_test():
     print('年龄为{age},生日为{birth}'.format(age=2, birth=1218))
     print('常量 PI 的值近似为 {0:.3f}。'.format(math.pi))  # 小数点后保留三位小数
     print('站点列表 {0}, {1}, 和 {other}。'.format('Google', 'baidu', other='Taobao'))
+    table = {'Google': 1, 'Runoob': 2, 'Taobao': 3}
+    print('Runoob: {0[Runoob]:d}; Google: {0[Google]:d}; Taobao: {0[Taobao]:d}'.format(table))  #可以保证其类型以及精确度
