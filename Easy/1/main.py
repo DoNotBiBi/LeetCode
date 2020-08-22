52c65f543f24caf535555ec7eeeeb55e39024766
wangkang <wangkang941218@outlook.com>
2020-08-22 22:53:26 +0800

    合并

--- Easy/1/main.py
+++ Easy/1/main.py
@@ -1,13 +1,20 @@
-# 1.题目描述：
-# 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
-# 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
-#
-# 示例:
-# 给定 nums = [2, 7, 11, 15], target = 9
-# 因为 nums[0] + nums[1] = 2 + 7 = 9
-# 所以返回 [0, 1]
 
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 class Solution:
+    # 1.题目描述：
+    # 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+    # 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+    #
+    # 示例:
+    # 给定 nums = [2, 7, 11, 15], target = 9
+    # 因为 nums[0] + nums[1] = 2 + 7 = 9
+    # 所以返回 [0, 1]
     def twoSum(self, nums, target: int):
         """
             :type nums: List[int]
@@ -22,25 +29,25 @@ class Solution:
             hashmap[num] = index
         return None
 
-
-# 题目描述
-# 给定一个二叉树，找出其最大深度。
-#
-# 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
-#
-# 说明: 叶子节点是指没有子节点的节点。
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-class Solution:
+    # 题目描述
+    # 给定一个二叉树，找出其最大深度。
+    #
+    # 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+    #
+    # 说明: 叶子节点是指没有子节点的节点。
+    # Definition for a binary tree node.
     def maxDepth(self, root: TreeNode) -> int:
         def top_down(node, h):
             return h if node is None else max(top_down(node.left, h + 1), top_down(node.right, h + 1))
+
         return top_down(root, 0)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     s = Solution()
