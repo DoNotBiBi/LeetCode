531567849aca32d5d7d73486f06031a754b4ee41
wangkang <wangkang941218@outlook.com>
2020-08-23 10:52:07 +0800

    合并

--- Easy/1/main.py
+++ Easy/1/main.py
@@ -1,11 +1,10 @@
-
-
-
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
+
+
 class Solution:
     # 1.题目描述：
     # 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
@@ -42,15 +41,59 @@ class Solution:
 
         return top_down(root, 0)
 
+    # 求树的最小深度
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        ans = 0
+        if not root.left and not root.right:  # 叶子节点
+            ans = 1
+        elif root.left and root.right:  # 左右子树均不为空
+            ans = min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        elif root.left:  # 左子树不为空 & 右子树为空
+            ans = self.minDepth(root.left) + 1
+        else:  # 左子树为空 & 右子树不为空
+            ans = self.minDepth(root.right) + 1
+        return ans
 
+    # 最长公共子串
+    def longestCommonPrefix(self, strs) -> str:
+        """
+        :param self:
+        :param strs: List[List[str]]
+        :return: str
+        """
+        if not strs:
+            return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i, x in enumerate(s1):
+            if x != s2[i]:
+                return s2[:i]
+        return s1
 
+    def longestCommonPrefix2(self, strs) -> str:
+        if not strs:
+            return ""
+        ss = list(map(set, zip(*strs)))
+        res = ""
+        for i, x in enumerate(ss):
+            x = list(x)
+            if len(x) > 1:
+                break
+            res = res + x[0]
+        return res
 
-
-
+    def longestCommonPrefix3(self, strs) -> str:
+        s = ""
+        for i in zip(*strs):
+            if len(set(i)) == 1:
+                s += i[0]
+            else:
+                break
+        return s
 
 
 if __name__ == '__main__':
-    s = Solution()
-    nums = [2, 7, 11, 15]
-    target = 18
-    print(s.twoSum(nums, target))
+    s = "helllo"
+    print(list(s))
