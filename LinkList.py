5bcb1f51e6e4f307bcd33060f9a920ce00bafe09
wangkang <wangkang941218@outlook.com>
2020-09-26 12:03:38 +0800

    hello

--- LinkList.py
+++ LinkList.py
@@ -2,6 +2,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
 
         pre_l = ListNode()
